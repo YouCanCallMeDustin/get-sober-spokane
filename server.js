3aff7e4671c405b55a06e9b9d99679f120adda08
LaughingLab <dustinshoemake@hotmail.com>
2025-07-14 20:02:40 -0700

    Update site: donations, map, navigation, and Google Maps integration

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ console.log('Starting server...');
 const express = require('express');
 const app = express();
 const PORT = 3001;
+const path = require('path');
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'docs')));
 
 app.get('/', (req, res) => {
   res.send('Hello World');
