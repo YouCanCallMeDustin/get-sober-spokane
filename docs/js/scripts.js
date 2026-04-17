20f991c99b199500904c348988e9bc8a5f437053
LaughingLab <dustinshoemake@hotmail.com>
2026-04-16 22:16:12 -0700

    PageSpeed

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -2,46 +2,6 @@
 * Start Bootstrap - Creative v7.0.8 (https://YOUR_USERNAME.github.io/sober-spokane)
 * Copyright 2013-2026 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-* Built: 2026-04-16T06:40:37.324Z
+* Built: 2026-04-17T05:12:37.449Z
 */
-//
-// Scripts
-// 
-
-window.addEventListener('DOMContentLoaded', event => {
-    // Remove scroll-based navbar shrink/fade behavior to keep navbar solid
-    // This intentionally does nothing so the navbar remains consistent
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link:not(.dropdown-toggle)')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
-    // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
-
-    // Video playlist logic removed: only a single combined video is used for the masthead background.
-
-    // Contact form custom handler
-    // (Removed for Formspree integration)
-
-});
+window.addEventListener("DOMContentLoaded",e=>{document.body.querySelector("#mainNav")&&new bootstrap.ScrollSpy(document.body,{target:"#mainNav",rootMargin:"0px 0px -40%"});const o=document.body.querySelector(".navbar-toggler");[].slice.call(document.querySelectorAll("#navbarResponsive .nav-link:not(.dropdown-toggle)")).map(function(e){e.addEventListener("click",()=>{"none"!==window.getComputedStyle(o).display&&o.click()})}),new SimpleLightbox({elements:"#portfolio a.portfolio-box"})});
